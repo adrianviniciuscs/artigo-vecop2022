1a4cdd4ed01bf7b8ecabba6839e4a2de1150c6cd
Adrian Vinicius <93016748+adrianviniciuscs@users.noreply.github.com>
2022-10-18 16:23:37 -0300

    Initial commit

---
+++ MLWebApp/ml_model.py
@@ -0,0 +1,12 @@
+import joblib
+model = joblib.load(r"MLWebApp\rfc_model.pkl")
+
+def prediction_model(ejection_fraction, serum_creatinine):
+    prediction = model.predict([[ejection_fraction, serum_creatinine]])
+    return prediction
+
+def prediction_model_proba(ejection_fraction, serum_creatinine):
+    proba = model.predict_proba([[ejection_fraction, serum_creatinine]])
+    return proba
+
+    
