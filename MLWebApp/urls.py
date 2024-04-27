fc1e178ac3963ed063739c6941aed65e65e745b7
adrianviniciuscs <adrianvinicius.cs@gmail.com>
2024-04-27 11:49:41 -0300

    organization

--- MLWebApp/urls.py
+++ MLWebApp/urls.py
@@ -13,11 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 from MLWebApp import views
 
 urlpatterns = [
-path('', views.home),
-path('result/', views.result , name='result')
-]
+        path('', views.home),
+        path('result/', views.result, name='result')
+        ]
--- MLWebApp/views.py
+++ MLWebApp/views.py
@@ -1,20 +1,17 @@
 from django.shortcuts import render
-import joblib
-import sklearn
-import pandas
 import numpy as np
 from MLWebApp.ml_model import prediction_model, prediction_model_proba
+
+
 def home(request):
     return render(request, 'index.html')
+
 
 def result(request):
     serum_creatinine = float(request.GET["serum_creatinine"])
     ejection_fraction = float(request.GET["ejection_fraction"])
-    result = prediction_model(ejection_fraction,serum_creatinine) 
+    result = prediction_model(ejection_fraction, serum_creatinine)
     proba = np.max(prediction_model_proba(ejection_fraction, serum_creatinine))
     print(result)
     print(proba)
-    return render(request, 'result.html', {'result': result,'proba': proba})
-
-
-
+    return render(request, 'result.html', {'result': result, 'proba': proba})
